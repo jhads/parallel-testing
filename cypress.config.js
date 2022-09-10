640b7ae01dc52efb41666a828849100ff6e24d15
Mark Hughes <m.hughes@heni.com>
2022-09-10 13:26:26 +0200

    new name

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,10 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'a7bq2k'
-})
+  projectId: '7q8bgj',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
